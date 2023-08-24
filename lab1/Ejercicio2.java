cbe47994efd45da920c42ac925aacbaa282dc430
soydz <soydz@pm.me>
2023-08-23 19:04:22 -0500

    Se soluciona error

--- lab1/Ejercicio2.java
+++ lab1/Ejercicio2.java
@@ -24,6 +24,8 @@ class Ejercicio2 {
             System.out.println("\n" + a + " Esta por encima del promedio");
         }else if(a < promedio){
             System.out.println("\n" + a + " Esta por debajo del promedio");
+        }else {
+            System.out.println("\n" + a + " Es igual al promedio");
         }
         
         int b = 50;
@@ -31,42 +33,60 @@ class Ejercicio2 {
             System.out.println("\n" + b + " Esta por encima del promedio");
         }else if(b < promedio){
             System.out.println("\n" + b + " Esta por debajo del promedio");
+        }else {
+            System.out.println("\n" + b + " Es igual al promedio");
         }
+
         
         int c = 60;
         if(c > promedio){
             System.out.println("\n" + c + " Esta por encima del promedio");
         }else if(c < promedio){
             System.out.println("\n" + c + " Esta por debajo del promedio");
+        }else {
+            System.out.println("\n" + c + " Es igual al promedio");
         }
+
         
         int d = 24;
         if(d > promedio){
             System.out.println("\n" + d + " Esta por encima del promedio");
         }else if(d < promedio){
             System.out.println("\n" + d + " Esta por debajo del promedio");
+        }else {
+            System.out.println("\n" + d + " Es igual al promedio");
         }
+
         
         int e = 66;
         if(e > promedio){
             System.out.println("\n" + e + " Esta por encima del promedio");
         }else if(e < promedio){
             System.out.println("\n" + e + " Esta por debajo del promedio");
+        }else {
+            System.out.println("\n" + e + " Es igual al promedio");
         }
+
         
         int f = 12;
         if(f > promedio){
             System.out.println("\n" + f + " Esta por encima del promedio");
         }else if(f < promedio){
             System.out.println("\n" + f + " Esta por debajo del promedio");
+        }else {
+            System.out.println("\n" + f + " Es igual al promedio");
         }
+
         
         int g = 98;
         if(g > promedio){
             System.out.println("\n" + g + " Esta por encima del promedio");
         }else if(g < promedio){
             System.out.println("\n" + g + " Esta por debajo del promedio");
+        }else {
+            System.out.println("\n" + g + " Es igual al promedio");
         }
+
 
     }
 }
